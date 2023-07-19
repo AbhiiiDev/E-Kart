16835a58715c912e6573371dd24bd3edc459335b
ABHISHEK VERMA <104015898+AbhiiiDev@users.noreply.github.com>
2023-07-19 16:31:01 +0530

    product-controllers setup complete

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -21,6 +21,24 @@ exports.getAllProducts=async(req,res)=>{
     })
 }
 
+//controller to get single product 
+exports.getSingleProduct=async(req,res,next)=>{
+    const product= await Product.findById(req.params.id)
+    
+    if(!product){
+        return res.status(500).json({
+            success:false,
+            message:"product not found"
+        })
+    }
+    res.status(200).json({
+        success:true,
+        product
+    })
+}
+
+
+
 //controller for update products  Admin-Route
 exports.updateProduct=async(req,res)=>{
     let product= await Product.findById(req.params.id)
