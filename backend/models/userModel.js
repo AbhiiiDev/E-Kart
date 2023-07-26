7a661d0d28400e0162d1bbab82760d233c71e72d
ABHISHEK VERMA <104015898+AbhiiiDev@users.noreply.github.com>
2023-07-27 00:53:10 +0530

    reset-password-token-completed

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose');
 const validator=require('validator');
-const bcrypt= require('bcryptjs');
-const jwt=require("jsonwebtoken")
+const jwt=require("jsonwebtoken");
+const crypto=require("crypto");
 
 const userSchema=new mongoose.Schema({
     name:{
@@ -63,4 +63,24 @@ userSchema.methods.comparePassword=async function(enteredPassword){
     return await bcrypt.compare(enteredPassword,this.password);
 }
       
+
+
+
+userSchema.methods.getResetToken=function(){
+    //generating token
+
+    const resetToken=crypto.randomBytes(20).toString("hex");
+    //20byte random token : hex:otherwise any other 
+
+    //hashing
+    this.resetPasswordToken=crypto.createHash("sha256").
+    update(resetToken)
+    .digest("hex");
+
+
+    this.resetPasswordExpire=Date.now()+15*60*1000;
+
+    return resetToken;
+}
+
 module.exports=mongoose.model("User",userSchema);
